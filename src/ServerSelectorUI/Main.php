e53679738c05d415adabd732cf97f61860f5a06f
iZeaoGamer <jameshayward14@icloud.com>
2018-11-11 11:40:13 +0000

    Finally fixed ServerSelectorUI Plugin.
    Configurations will arrive in the near future. Keep an eye on this repo!

--- src/ServerSelectorUI/Main.php
+++ src/ServerSelectorUI/Main.php
@@ -2,112 +2,93 @@
 
 namespace ServerSelectorUI;
 
-use pocketmine\{Server, Player};
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\block\{BlockBreakEvent, BlockPlaceEvent};
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\player\{PlayerJoinEvent, PlayerInteractEvent, PlayerExhaustEvent, PlayerDropItemEvent, PlayerItemConsumeEvent};
 use pocketmine\utils\TextFormat;
-use pocketmine\command\{Command, CommandSender, CommandExecutor, ConsoleCommandSender};
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\item\Item;
 
 class Main extends PluginBase implements Listener {
-	
     public function registerEvents(): void {
 	    $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-    public function onEnable(): void {
+    public function onEnable() : void {
 	    $this->registerEvents();
 		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
 		if($api === null){
 			$this->getServer()->getPluginManager()->disablePlugin($this);			
 		}
     }
-    /*public function onDisable(): void { Coming soon
-        foreach ($this->getServer()->getOnlinePlayers() as $players) {
-            $players->transfer("hub.voidminerpe.ml", "25584"); To-Do make this configurable.
-        }
-    }*/
-    public function onDamageDisable(EntityDamageEvent $event): void {
+    public function onDamageDisable(EntityDamageEvent $event){
         if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
             $event->setCancelled(true);
         }
     }
-  public function onPlaceDisable(BlockPlaceEvent $event): void {
+  public function onPlaceDisable(BlockPlaceEvent $event) {
         $event->setCancelled(true);
     }
-    public function onBreakDisable(BlockBreakEvent $event): void {
+    public function onBreakDisable(BlockBreakEvent $event) {
 		$event->setCancelled(true);
     }
-    public function HungerDisable(PlayerExhaustEvent $event): void {
+    public function HungerDisable(PlayerExhaustEvent $event) {
         $event->setCancelled(true);
     }
-    public function DropItemDisable(PlayerDropItemEvent $event): void {
+    public function DropItemDisable(PlayerDropItemEvent $event){
         $event->setCancelled(true);
     }
-    public function onConsumeDisable(PlayerItemConsumeEvent $event): void {
+    public function onConsumeDisable(PlayerItemConsumeEvent $event){
         $event->setCancelled(true);
     }
-    public function onJoin(PlayerJoinEvent $event): void {
+    public function onJoin(PlayerJoinEvent $event){
 	    $player = $event->getPlayer();
-	    $this->LoadItems($player);
+	     $player->getInventory()->setItem(2, Item::get(345)->setCustomName("§a§lServer Selector"));
     }
-    public function LoadItems(Player $player): void {
-	    $player->getInventory()->clearAll();
-	    $player->getInventory()->setItem(2, Item::get(345)->setCustomName("§a§lServer Selector"));
-    }
-    public function onInteract(PlayerInteractEvent $event) {
+    public function onInteract(PlayerInteractEvent $event){
 	   $player = $event->getPlayer();
 	    $item = $player->getInventory()->getItemInHand();
 	    if($item->getCustomName() == "§a§lServer Selector"){
-		                        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
 					$form = $api->createSimpleForm(function (Player $sender, $data){
-					$result = $data[0];
-					
-					if($result === null){
-						return true;
+					 $result = $data;
+					if($result != null) {
 					}
 						switch($result){
 							case 0:
-								$sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
-								$sender->transfer("factions.voidminerpe.ml", "25655");
+							    $sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
+								$command = "transferserver factions.voidminerpe.ml 25655";
+								$this->getServer()->getCommandMap()->dispatch($sender, $command);
 								$sender->sendMessage(TextFormat::RED . "Something went wrong - Contact server administrators if this was a mistake.");
 							break;
-							
-							switch($result){
+								
 							case 1:
-								$sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
-								$sender->transfer("factions2.voidminerpe.ml", "25584");
+							    $sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
+								$command = "transferserver factions2.voidminerpe.ml 25584";
+								$this->getServer()->getCommandMap()->dispatch($sender, $command);
 								$sender->sendMessage(TextFormat::RED . "Something went wrong - Contact server administrators if this was a mistake.");
 						        break;
-									
-						    switch($result){
+							
 							case 2:
 								$sender->sendMessage(TextFormat::RED . "Coming soon");
 								//$command = "";
 								//$this->getServer()->getCommandMap()->dispatch($player, $command);
-								return true;
 							break;
+              
 								
-					        switch($result){
-							case 3:
-								$sender->sendMessage("§cYou have closed the server selector!");
-								return true;
-							break;
-					
-					        		}
-								
-							}
 						}
-					}
 					});
 					$form->setTitle("§a§lServer Selector!");
 					$form->setContent("§bPlease choose a server to teleport to!");
-					$form->addButton(TextFormat::BOLD . "§3OP §bFactions\n§a§lONLINE");
-					$form->addButton(TextFormat::BOLD . "§3Normal §bFactions\n§a§lONLINE");
-					$form->addButton(TextFormat::BOLD . "§5Prisons\n§c§lComing Soon");
-		           		$form->addButton(TextFormat::BOLD . "§c§lEXIT");
+					$form->addButton(TextFormat::BOLD . "§3OP §bFactions\n§a§lONLINE", 0);
+					$form->addButton(TextFormat::BOLD . "§3Normal §bFactions\n§a§lONLINE", 1);
+					$form->addButton(TextFormat::BOLD . "§5Prisons\n§c§lComing Soon", 2);
 					$form->sendToPlayer($player);
 	    }
     }
@@ -124,43 +105,28 @@ class Main extends PluginBase implements Listener {
 					}
 						switch($result){
 							case 0:
-								$sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
-								$sender->transfer("factions.voidminerpe.ml", "25655");
-								$sender->sendMessage(TextFormat::RED . "Something went wrong - Contact server administrators if this was a mistake.");
+								$command = "transferserver voidprisonspe.ml 25647";
+								$this->getServer()->getCommandMap()->dispatch($sender, $command);
 							break;
 								
-						switch($result){	
 							case 1:
-								$sender->sendMessage(TextFormat::DARK_PURPLE . "Please wait whilst we transferred you to the server..");
-								$sender->transfer("factions2.voidminerpe.ml", "25584");
-								$sender->sendMessage(TextFormat::RED . "Something went wrong - Contact server administrators if this was a mistake.");
+								$command = "transferserver voidfactionspe.ml 19132";
+								$this->getServer()->getCommandMap()->dispatch($sender, $command);
 						        break;
-									
-						switch($result){
+							
 							case 2:
-								$sender->sendMessage(TextFormat::RED . "Coming soon");
-								//$command = "";
-								//$this->getServer()->getCommandMap()->dispatch($player, $command);
-								return true;
+								$command = "transferserver voidkitpvppe.ml 25625";
+								$this->getServer()->getCommandMap()->dispatch($sender, $command);
 							break;
+              
 								
-						switch($result){
-							case 3:
-								$sender->sendMessage("§cYou have closed the server selector!");
-								return true;
-							break;
-								}
-							}
 						}
-								
-					}
 					});
 					$form->setTitle("§a§lServer Selector!");
 					$form->setContent("§bPlease choose a server to teleport to!");
-					$form->addButton(TextFormat::BOLD . "§3OP §bFactions\n§a§lONLINE");
-					$form->addButton(TextFormat::BOLD . "§3Normal §bFactions\n§a§lONLINE");
-					$form->addButton(TextFormat::BOLD . "§5Prisons\n§c§lComing Soon");
-		                        $form->addButton(TextFormat::BOLD . "§c§lEXIT");
+					$form->addButton(TextFormat::BOLD . "§6§lVoid§bPrisons§cPE (§dTap Me!)");
+					$form->addButton(TextFormat::BOLD . "§6§lVoid§bFactions§cPE (§dTap Me!)");
+					$form->addButton(TextFormat::BOLD . "§6§lVoid§bKitPvP§cPE (§dTap me!)");
 					$form->sendToPlayer($sender);
 				}
 				else{
